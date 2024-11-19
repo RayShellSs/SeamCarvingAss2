cad3c70eba4e379167237e25fbfde3d86efa4143
RayShellSs <ritoverycrazyy@gmail.com>
2024-11-20 00:38:03 +0800

    save first

--- SeamCarvingAss2.h
+++ SeamCarvingAss2.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include "resource.h"
+#include <vector>
 
 void OpenImage(HWND hwnd);
 void ResizeImage(HWND hwnd, int n, int m);
 void SaveImage(HWND hwnd);
+
+//std::vector<int> findVerticalSeamGreedy(const Mat& energyMap);
+//Mat removeVerticalSeamGreedy(const Mat& image, const std::vector<int>& seam);
+//std::vector<int> findHorizontalSeamGreedy(const Mat& energyMap);
+//Mat removeHorizontalSeamGreedy(const Mat& image, const std::vector<int>& seam);
